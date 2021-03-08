5eeb022b2662e0b57a14c70064cb506f4f00e9fc
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-03-08 18:49:07 +0000

    Added Controller interface

---
+++ src/main/java/csc1035/project2/IController.java
@@ -0,0 +1,19 @@
+package csc1035.project2;
+
+import java.util.List;
+
+public interface IController<E> {
+    void save(E e); // Create
+
+    void update(E s); // Update
+
+    E getById(Class<E> c, int id); // Read
+
+    E getById(Class<E> c, String id); // Read String Id
+
+    List<E> getAll(Class<E> c); // Supporting method for retrieving all entries in a database table
+
+    void delete(Class<E> c,int id); // Delete
+
+    void bulkListSave(List<E> e); // Bulk save a list collection of entries
+}
