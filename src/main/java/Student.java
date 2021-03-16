abc367801c8659eb8ac8ecbc1dd1ecdde95f5b95
c0044514 <jwfrost2002@gmail.com>
2021-03-16 18:25:17 +0000

    Initial commit. Add constructor for Student

---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    public String studentId;
+    public String firstName;
+    public String lastName;
+
+    public Student(String studentId, String firstName, String lastName) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
