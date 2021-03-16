1df7189ff1c9827e194187276f456bc8e40bf83e
c0044514 <jwfrost2002@gmail.com>
2021-03-16 20:32:04 +0000

    In Student.java make all attributes private, add hibernate annotations and empty constructor, generate getters and setters

--- src/main/java/csc1035/project2/Student.java
+++ src/main/java/csc1035/project2/Student.java
@@ -1,14 +1,52 @@
 package csc1035.project2;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Student")
 public class Student {
 
-    public String studentId;
-    public String firstName;
-    public String lastName;
+    @Id
+    @Column(name = "student_id")
+    private String studentId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Student() {
+    }
 
     public Student(String studentId, String firstName, String lastName) {
         this.studentId = studentId;
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public String getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(String newStudentId) {
+        this.studentId = newStudentId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+
 }
