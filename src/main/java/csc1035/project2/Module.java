72cc7dc7e39729db0c4173833c60c02ee917ff33
lestherll <ljllacuna5@gmail.com>
2021-03-18 05:00:59 +0000

    Init Module model

--- src/main/java/csc1035/project2/Module.java
+++ src/main/java/csc1035/project2/Module.java
@@ -1,4 +1,26 @@
 package csc1035.project2;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Module")
 public class Module {
+
+    @Id
+    @Column(name = "module_id")
+    private String moduleId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "credits")
+    private int credits;
+
+    @Column(name = "weeks")
+    private int weeks;
 }
+
+
