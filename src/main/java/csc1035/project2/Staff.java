5188c2b9afb79b67d16760455190d77f8c1f9146
lestherll <ljllacuna5@gmail.com>
2021-03-18 05:01:13 +0000

    Init Staff model

--- src/main/java/csc1035/project2/Staff.java
+++ src/main/java/csc1035/project2/Staff.java
@@ -1,4 +1,22 @@
 package csc1035.project2;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 public class Staff {
+
+    @Id
+    @Column(name = "staff_id")
+    private String staffId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy = "staff")
+    private List<Booking> booking;
 }
