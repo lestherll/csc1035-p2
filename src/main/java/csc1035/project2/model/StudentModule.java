d125b3c516c42497b23ca492742411b9acb9f136
benmi <ben>
2021-03-21 16:44:26 +0000

    changed variable names and created hashcode as well as unique check

--- src/main/java/csc1035/project2/model/StudentModule.java
+++ src/main/java/csc1035/project2/model/StudentModule.java
@@ -16,33 +16,61 @@ public class StudentModule implements Serializable {
 
     @Id
     @Column(name="module_id")
-    private String module_id;
+    private String moduleId;
 
     @Id
     @Column(name="student_id")
-    private String student_id;
+    private String studentId;
 
     public StudentModule() {}
 
     public StudentModule(String module_id, String student_id) {
-        this.module_id = module_id;
-        this.student_id = student_id;
+        this.moduleId = module_id;
+        this.studentId = student_id;
     }
 
-    public String getModule_id() {
-        return module_id;
+    public String getModuleId() {
+        return moduleId;
     }
 
-    public void setModule_id(String module_id) {
-        this.module_id = module_id;
+    public void setModuleId(String moduleId) {
+        this.moduleId = moduleId;
     }
 
-    public String getStudent_id() {
-        return student_id;
+    public String getStudentId() {
+        return studentId;
     }
 
-    public void setStudent_id(String student_id) {
-        this.student_id = student_id;
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModule{" +
+                "moduleId='" + moduleId + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return (int)(this.studentId + this.moduleId).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof StudentModule)) return false;
+        StudentModule pk = (StudentModule) obj;
+        return pk.studentId.equals(this.studentId) && pk.moduleId.equals(this.moduleId);
+    }
+
+
+    public List<StaffModule> getBookingsThisWeek(Session session) {
+        return session
+                .createNamedQuery("staffModule_getAll", StaffModule.class)
+                .getResultList();
     }
 
 }
