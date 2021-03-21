56159eab9dde085f40c0af0582e32bdf08b05533
benmi <ben>
2021-03-21 00:15:14 +0000

    Added comments and JavaDoc for Module class

--- src/main/java/csc1035/project2/model/Module.java
+++ src/main/java/csc1035/project2/model/Module.java
@@ -1,11 +1,21 @@
 package csc1035.project2.model;
-
 import org.hibernate.Session;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+/**
+ * This is the module class when booking a room there needs to be a module that the room is booked for
+ * The module class contains all need information about the ID, Name, Duration and Credits awarded for the module.
+ *
+ * @auther
+ * JavaDoc and comments by Ben Mitchell
+ *
+ *
+ * "Module_getAll" query will return all of the Modules from the Module Table
+ *
+ */
 
 @Entity
 @Table(name = "Module")
@@ -13,7 +23,7 @@ import java.util.Set;
         @NamedQuery(name = "Module_getAll", query = "FROM Module")
 })
 public class Module {
-
+    //Create variables and assign them to the columns in the Module Table
     @Id
     @Column(name = "module_id")
     private String moduleId;
@@ -27,6 +37,7 @@ public class Module {
     @Column(name = "weeks")
     private int weeks;
 
+    // Create all the relationships to the other tables in the database
     @OneToMany(mappedBy = "module")
     private List<Booking> booking;
 
@@ -38,6 +49,14 @@ public class Module {
 
     public Module() {}
 
+    /**
+     *
+     * @param moduleId This is the identifier for the Module
+     * @param name this is the name of the Module
+     * @param credits This is how many credits will be awarded for the module
+     * @param weeks This is the duration that the module will last
+     *
+     */
     public Module(String moduleId, String name, int credits, int weeks) {
         this.moduleId = moduleId;
         this.name = name;
@@ -45,6 +64,7 @@ public class Module {
         this.weeks = weeks;
     }
 
+    //Create all of the get and setter methods so yuo are able to set and return individual attributes
     public String getModuleId() {
         return moduleId;
     }
@@ -102,6 +122,13 @@ public class Module {
     }
 
     // Query Methods
+
+    /**
+     *
+     * @param session Creates Database Session
+     * @return returns all of the modules in the module Table
+     */
+
     public static List<Module> getAll(Session session) {
         return session
                 .createNamedQuery("Module_getAll", Module.class)
