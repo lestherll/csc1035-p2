7557b306d9971984ec4b9b9cb6c8c622bfec89a6
benmi <ben>
2021-03-21 14:50:11 +0000

    Added toString() and named query to StaffModule as well as creating all of the getters and setters for the class

--- src/main/java/csc1035/project2/model/StaffModule.java
+++ src/main/java/csc1035/project2/model/StaffModule.java
@@ -1,5 +1,4 @@
 package csc1035.project2.model;
-
 import org.hibernate.Session;
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,6 +9,9 @@ import java.util.Set;
 @Entity
 @Table(name = "StudentModule")
 @IdClass(StudentModule.class)
+@NamedQueries({
+        @NamedQuery(name = "staffModule_getAll", query = "FROM StaffModule"),
+})
 
 public class StaffModule implements Serializable {
     //Create variables, assign them to the columns to the staff_module Table and set Primary key field.
@@ -27,4 +29,38 @@ public class StaffModule implements Serializable {
         this.module_id = module_id;
         this.staff_id = staff_id;
     }
+
+    public String getModule_id() {
+        return module_id;
+    }
+
+    public String getStaff_id() {
+        return staff_id;
+    }
+
+    public void setModule_id(String module_id) {
+        this.module_id = module_id;
+    }
+
+    public void setStaff_id(String staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    /**
+     *
+     * @return Returns human readable print of the StaffModule Class
+     */
+    @Override
+    public String toString() {
+        return "StaffModule{" +
+                "module_id='" + module_id + '\'' +
+                ", staff_id='" + staff_id + '\'' +
+                '}';
+    }
+        public List<StaffModule> getBookingsThisWeek(Session session) {
+            return session
+                    .createNamedQuery("staffModule_getAll", StaffModule.class)
+                    .getResultList();
+        }
+    
 }
