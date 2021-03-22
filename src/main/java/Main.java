4e98e439d3a1fe772d20e0e2e7b56a2d93b5deff
lestherll <ljllacuna5@gmail.com>
2021-03-22 02:08:31 +0000

    Added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import csc1035.project2.io.UserInterface;
+import csc1035.project2.model.*;
+import csc1035.project2.controller.*;
+import csc1035.project2.model.Module;
+import org.hibernate.SessionFactory;
+import csc1035.project2.util.HibernateUtil;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        IController ic = new Controller(sf);
+        Scanner sc = new Scanner(System.in);
+
+        UserInterface ui = new UserInterface(sc, ic, sf);
+        ui.main();
+    }
+}
