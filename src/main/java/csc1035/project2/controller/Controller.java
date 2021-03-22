6f736879dfceb598fee82051ac936add583e0516
lestherll <ljllacuna5@gmail.com>
2021-03-22 10:13:32 +0000

    Javadocs for Controller.java

--- src/main/java/csc1035/project2/controller/Controller.java
+++ src/main/java/csc1035/project2/controller/Controller.java
@@ -5,15 +5,26 @@ import org.hibernate.SessionFactory;
 
 import java.util.List;
 
-
+/**
+ * A CRUD Controller that implements the IContoller interface
+ * @param <E> the class parameter for the controller
+ */
 public class Controller<E> implements IController<E>{
 
     private final SessionFactory sessionFactory;
 
+    /**
+     * Injects a sessionFactory object for the controller to use
+     * @param sessionFactory will allows the controller to open sessions
+     */
     public Controller(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
+    /**
+     * Saves an object to a database table
+     * @param o the object to be saved
+     */
     @Override
     public void save(Object o) {
         try (Session session = this.sessionFactory.openSession()) {
@@ -26,6 +37,10 @@ public class Controller<E> implements IController<E>{
 
     }
 
+    /**
+     * Updates the object's fields
+     * @param s entity that will be updated
+     */
     @Override
     public void update(Object s) {
         try (Session session = this.sessionFactory.openSession()) {
@@ -38,20 +53,30 @@ public class Controller<E> implements IController<E>{
 
     }
 
+    /**
+     * Update multiple objects from the list e
+     * @param e list of entities that will be updated or saved
+     */
     @Override
-    public void bulkListUpdate(List<E> entityList) {
+    public void bulkListUpdate(List<E> e) {
         try (Session session = this.sessionFactory.openSession()) {
             session.beginTransaction();
-            for (E e: entityList) {
-                session.saveOrUpdate(e);
+            for (E entry: e) {
+                session.saveOrUpdate(entry);
             }
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
     }
 
+    /**
+     * Read from the database table using class c and int id
+     * @param c dictates what class the entity is
+     * @param id the integer id that the read will be based on
+     * @return the object that was read
+     */
     @Override
     public E getById(Class<E> c, int id) {
         E entry = null;
@@ -65,6 +90,12 @@ public class Controller<E> implements IController<E>{
         return entry;
     }
 
+    /**
+     * Read from the database table using class c and string id
+     * @param c dictates what class the entity is
+     * @param id the String id that the read will be based on
+     * @return the object that was read
+     */
     @Override
     public E getById(Class<E> c, String id) {
         E entry = null;
@@ -79,6 +110,11 @@ public class Controller<E> implements IController<E>{
         return entry;
     }
 
+    /**
+     * Read all records from a table
+     * @param c the class to be fetched
+     * @return the list of objects that was read
+     */
     @Override
     public List<E> getAll(Class<E> c) {
         List<E> entries = null;
@@ -93,6 +129,11 @@ public class Controller<E> implements IController<E>{
         return entries;
     }
 
+    /**
+     * Delete a record based on its integer id
+     * @param c the class of the entity to be deleted
+     * @param id the integer id of the entity to be deleted
+     */
     @Override
     public void delete(Class<E> c, int id) {
         try (Session session = this.sessionFactory.openSession()) {
@@ -107,6 +148,11 @@ public class Controller<E> implements IController<E>{
 
     }
 
+    /**
+     * Delete a record based on its string id
+     * @param c the class of the entity to be deleted
+     * @param id the string id of the entity to be deleted
+     */
     @Override
     public void delete(Class<E> c, String id) {
         try (Session session = this.sessionFactory.openSession()) {
@@ -119,16 +165,20 @@ public class Controller<E> implements IController<E>{
         }
     }
 
+    /**
+     * Bulks saves entries from a list
+     * @param e the list of entities to be saved
+     */
     @Override
-    public void bulkListSave(List<E> entity) {
+    public void bulkListSave(List<E> e) {
         try (Session session = this.sessionFactory.openSession()) {
             session.beginTransaction();
-            for (E entry: entity){
+            for (E entry: e){
                 session.save(entry);
             }
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
 
