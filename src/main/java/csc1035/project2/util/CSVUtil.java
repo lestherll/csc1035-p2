134e14420e4b9649184e985bda6450729050fdf6
lestherll <ljllacuna5@gmail.com>
2021-03-22 10:32:43 +0000

    Javadocs for CSVUtil.java

--- src/main/java/csc1035/project2/util/CSVUtil.java
+++ src/main/java/csc1035/project2/util/CSVUtil.java
@@ -12,8 +12,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * CSVUtil is the helper class for reading csv files
+ * from resources
+ */
 public class CSVUtil {
 
+    /**
+     * the main use of CSVUtil is to call this method
+     * which reads a csv file from a given path
+     * @param path the path of the file
+     * @return a nested List of strings split by comma ","
+     */
     public static List<List<String>> read(String path) {
         List<List<String>> result = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(path))) {
@@ -26,6 +36,11 @@ public class CSVUtil {
         return result;
     }
 
+    /**
+     * Calls bulkListUpdate on all csv files read using the
+     * different read methods
+     * @param ic the CRUD controller that will save the entries
+     */
     public static void saveOrUpdateFromCSV(IController ic) {
         ic.bulkListUpdate(readRoom());
         ic.bulkListUpdate(readModule());
@@ -35,6 +50,11 @@ public class CSVUtil {
         ic.bulkListUpdate(readStudentModule());
     }
 
+    /**
+     * Calls bulkListSave on all csv files read using the different
+     * read methods
+     * @param ic the CRUD controller that will save the entries
+     */
     public static void saveFromCSV(IController ic) {
         ic.bulkListSave(readRoom());
         ic.bulkListSave(readModule());
@@ -44,18 +64,33 @@ public class CSVUtil {
         ic.bulkListSave(readStudentModule());
     }
 
+    /**
+     * Helper method for printing a list of some object/class
+     * @param c the class of the objects in the list
+     * @param data the list that contains the data of c class
+     * @param <E>
+     */
     public static <E> void printList(Class<E> c, List<E> data) {
         for (E e: data) {
             System.out.println(e);
         }
     }
 
+    /**
+     * Helper method for pringting a nested list of strings
+     * @param data the nested list of strings
+     */
     public static void printList(List<List<String>> data) {
         for (List<String> list: data) {
             System.out.println(list);
         }
     }
 
+    /**
+     * Reads rooms.csv file and creates a list
+     * of Room objects out of it
+     * @return list of Room
+     */
     public static List<Room> readRoom() {
         String path = "src/main/resources/rooms.csv";
         List<List<String>> res = read(path);
@@ -74,6 +109,11 @@ public class CSVUtil {
         return list;
     }
 
+    /**
+     * Reads staff.csv file and creates a list
+     * of Staff objects out of it
+     * @return list of Staff
+     */
     public static List<Staff> readStaff() {
         String path = "src/main/resources/staff.csv";
         List<List<String>> res = read(path);
@@ -87,6 +127,11 @@ public class CSVUtil {
         return list;
     }
 
+    /**
+     * Reads students.csv file and creates a list
+     * of Student objects
+     * @return list of Students
+     */
     public static List<Student> readStudent() {
         String path = "src/main/resources/students.csv";
         List<List<String>> res = read(path);
@@ -100,6 +145,11 @@ public class CSVUtil {
         return list;
     }
 
+    /**
+     * Reads modules.csv file and creates a list
+     * of Module objects
+     * @return list of Modules
+     */
     public static List<Module> readModule() {
         String path = "src/main/resources/modules.csv";
         List<List<String>> res = read(path);
@@ -123,6 +173,11 @@ public class CSVUtil {
         return list;
     }
 
+    /**
+     * Reads staff_module.csv and creates a list
+     * of StaffModule objects
+     * @return list of StaffModules
+     */
     public static List<StaffModule> readStaffModule() {
         String path = "src/main/resources/staff_module.csv";
         List<List<String>> res = read(path);
@@ -138,6 +193,11 @@ public class CSVUtil {
         return list;
     }
 
+    /**
+     * Reads student_module.csv and creates a list
+     * of StudentModule objects
+     * @return list of StudentModules
+     */
     public static List<StudentModule> readStudentModule() {
         String path = "src/main/resources/student_module.csv";
         List<List<String>> res = read(path);
